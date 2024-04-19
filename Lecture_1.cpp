551810585817b4da7cffde35016afcb286a6630f
Swapnil Bera <87073046+swapnilbera@users.noreply.github.com>
2024-04-19 21:19:01 +0530

    Create Lecture_1.cpp

---
+++ Lecture_1.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main() {
+  int a=2;
+  int b=3;
+  bool f=a!=b;
+  cout << f;
+  // return 0;
+}
+
+/*
+g++ main.cpp -o output   # Compile the main.cpp file and create an executable named "output"
+./output                 # Run the executable
+
+*/
