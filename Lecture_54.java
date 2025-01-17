7bc8b7d20bbac857e77720afe2718fb3c62a7e21
Swapnil Bera <87073046+swapnilbera@users.noreply.github.com>
2025-01-17 23:44:51 +0530

    Create Lecture_54.java

---
+++ Lecture_54.java
@@ -0,0 +1,48 @@
+public class stack_array {
+    public static void main(String[] args) {
+        stack s=new stack();
+        s.setsize(5);
+        s.push(1);
+        s.push(2);
+        s.push(3);
+        s.push(4);
+        s.push(5);
+        s.print();
+        s.pop();
+        s.pop();
+        s.pop();
+        s.print();
+
+    }
+}
+class stack{
+    int arr[];
+    int size;
+    int top=-1;
+    void setsize(int size){
+        this.size=size;
+        arr=new int[size];
+    }
+    void push(int element){
+        if(top!=size-1){
+            top++;
+            arr[top]=element;
+        }
+        else{
+            System.out.println("Stack Overflow....");
+        }
+    }
+    void pop(){
+        if(top!=-1){
+            top--;
+        }
+        else{
+            System.out.println("Stack Underflow....");
+        }
+    }
+    void print(){
+        for (int i = 0; i <= top; i++) {
+            System.out.println("Element is: "+arr[i]);
+        }
+    }
+}
