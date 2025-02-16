c454aa9c3c6226bf4a3d69fce68a165286087559
Swapnil Bera <87073046+swapnilbera@users.noreply.github.com>
2025-02-16 10:38:51 +0530

    Binary Tree in Java

---
+++ Lecture_62.java
@@ -0,0 +1,56 @@
+import java.util.*;
+class tree{
+    int data;
+    tree left;
+    tree right;
+    public tree(int data){
+        this.data=data;
+        left=null;
+        right=null;
+    }
+}
+public class lecture_62 {
+    public static tree create_tree(){
+        System.out.println("Enter Node Data: ");
+        int data;
+        Scanner sc=new Scanner(System.in);
+        data=sc.nextInt();
+        tree root=new tree(data);
+        if(data == -1) {
+            return null;
+        }
+        System.out.println("Enter data for inserting in left of " + data);
+        root.left=create_tree();
+        System.out.println("Enter data for inserting in right of " +data);
+        root.right=create_tree();
+        return root;
+    }
+    public static void tree_level_order_traversal(tree node){
+        Queue<tree> queue=new java.util.LinkedList<>();
+        queue.add(node);
+        queue.add(null);
+        while (!queue.isEmpty()){
+            tree temp=queue.remove();
+            if(temp==null){
+                System.out.println();
+                if(!queue.isEmpty())
+                    queue.add(null);
+            }
+            else {
+                System.out.print(temp.data + " ");
+                if (temp.left != null) {
+                    queue.add(temp.left);
+                }
+                if (temp.right != null) {
+                    queue.add(temp.right);
+                }
+            }
+        }
+
+    }
+
+    public static void main(String[] args) {
+        tree main_tree=create_tree();
+        tree_level_order_traversal(main_tree);
+    }
+}
